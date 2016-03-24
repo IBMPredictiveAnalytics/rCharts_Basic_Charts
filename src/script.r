fbc28cf178b4946eecddf7906d59ca9976cb4100
gfilla <gfilla23@gmail.com>
2016-03-24 12:21:46 -0500

    update v18 and R 3.2
    installed additional package dependencies.  Also made custom function
    for temp files because this broke in 3.2 for some reason.

--- src/script.r
+++ src/script.r
@@ -1,5 +1,5 @@
 # Install packages   -Need to get rCharts from GitHub - no binary for 3.1
-list.of.packages <- c( "devtools", 'RCurl')
+list.of.packages <- c( "devtools", 'RCurl', 'base64enc', 'rjson')
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -20,7 +20,23 @@ use_custom_directory <- %%use_custom_directory%%
       stop("Please provide a valid file path to save the output")
     }
   } else {
-    filepath <- paste( tempfile(), ".htm", sep='')
+     #filepath <- paste( tempfile(), ".htm", sep='')
+
+makeName <- function(n=2, len=10){
+  empty.str <- c(1:n)                  
+  for (i in 1:n)
+  {
+    empty.str[i] <- paste(sample(c(0:9, letters, LETTERS),
+                                    len, replace=TRUE),
+                             collapse="")
+  }
+  tmp <- Sys.getenv('TEMP')
+  dir.create(paste0(tmp,"\\",empty.str[1]))
+  full.path <- paste0(tmp,"\\",empty.str[1],"\\",empty.str[2],".htm")
+  return(full.path)
+}
+
+filepath <- makeName()
   }
 
 
@@ -29,7 +45,7 @@ use_custom_directory <- %%use_custom_directory%%
 working_dir <- dirname(filepath)
 setwd(working_dir)
 setwd("~/")
-print(names(modelerData))
+#print(names(modelerData))
 df <-data.frame(modelerData)
 xx<-df['%%Xaxis%%']
 yy<-df['%%Yaxis%%']
@@ -53,53 +69,53 @@ if('%%category_field%%' != ''){
     d1 <- dPlot(x ='%%Xaxis%%', y = '%%Yaxis%%',groups='%%category_field%%',data = df2, type = chart.type)
     
   }
-  if(chart.type == 'point')  {
-    d1 <- rPlot(%%Yaxis%%  ~%%Xaxis%%,
-                color ='%%category_field%%',
-                data = df2, 
-                type =chart.type)
-    d1$chart(color = %%Xaxis%%)
-  }
-  if(chart.type == 'line')  {
-    d1 <- rPlot('%%Xaxis%%','%%Yaxis%%',
-                color ='%%category_field%%',
-                data = df2, 
-                type =chart.type)
-    d1$chart(color = %%Xaxis%%)
-  }
-}else{
-  df2 <- data.frame(%%Xaxis%%, %%Yaxis%%)
-  if(chart.type != 'point'){
-    d1 <- dPlot(x ='%%Xaxis%%', y = '%%Yaxis%%',data = df2, type = chart.type)
-  }  else{
-    d1 <- dPlot( %%Yaxis%% ~ %%Xaxis%%,
-                 data = df2,
-                 type = "bubble"
-    )
-  }
+    if(chart.type == 'point')  {
+      d1 <- rPlot(%%Yaxis%%  ~%%Xaxis%%,
+                  color ='%%category_field%%',
+                  data = df2, 
+                  type =chart.type)
+      d1$chart(color = %%Xaxis%%)
+    }
+        if(chart.type == 'line')  {
+      d1 <- rPlot('%%Xaxis%%','%%Yaxis%%',
+                  color ='%%category_field%%',
+                  data = df2, 
+                  type =chart.type)
+      d1$chart(color = %%Xaxis%%)
+    }
+    }else{
+      df2 <- data.frame(%%Xaxis%%, %%Yaxis%%)
+      if(chart.type != 'point'){
+           d1 <- dPlot(x ='%%Xaxis%%', y = '%%Yaxis%%',data = df2, type = chart.type)
+       }  else{
+            d1 <- dPlot( %%Yaxis%% ~ %%Xaxis%%,
+            data = df2,
+            type = "bubble"
+            )
 }
+    }
 
-d1$guides (
+ d1$guides (
   x = list(
     min = pretty(df2$%%Xaxis%%)[1],
     max = tail(pretty(df2$%%Xaxis%%),1))
-  ,  
+,  
   y = list(
-    min = pretty(df2$%%Yaxis%%)[1],
-    max = tail(pretty(df2$%%Yaxis%%),1))
+  min = pretty(df2$%%Yaxis%%)[1],
+  max = tail(pretty(df2$%%Yaxis%%),1))
 )
-d1$legend(
-  x = 60,
-  y = 10,
-  width = 700,
-  height = 20,
-  horizontalAlign = "left"
-)
+    d1$legend(
+      x = 60,
+      y = 10,
+      width = 700,
+      height = 20,
+      horizontalAlign = "left"
+    )
 
-d1$save(filepath)
+d1$save(filepath, cdn = %%cdnflag%%)
 print("saved!")  
 browseURL(filepath)
-
+    
 
 
 
